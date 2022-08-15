2cec79eb1d58a15fdffa40f03de2c6ef482db17e
Grace Akpan <opps4lrng@gmail.com>
2022-08-14 18:30:42 -0600

    removed older handleApi method

--- controllers/user.js
+++ controllers/user.js
@@ -24,38 +24,6 @@ const handleApiCall = (req, res) => {
     .catch(() => res.status(400).json("unable to work with API"));
 };
 
-const handleApiCall2 = (req, res) => {
-  const { input } = req.body;
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: process.env.USER_ID,
-      app_id: process.env.APP_ID,
-    },
-    inputs: [{ data: { image: { url: input } } }],
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Key " + process.env.PAT,
-    },
-    body: raw,
-  };
-
-  fetch(
-    "https://api.clarifai.com/v2/models/" +
-      process.env.MODEL_ID +
-      "/versions/" +
-      process.env.MODEL_VERSION_ID +
-      "/outputs",
-    requestOptions
-  )
-    .then((data) => data.json())
-    .then((result) => res.send(result))
-    .catch(() => res.status(400).json("unable to work with API"));
-};
-
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
